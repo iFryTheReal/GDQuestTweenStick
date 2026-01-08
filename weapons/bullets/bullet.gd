36f304deea094851ef1a0fcda9c8d43833a6e8b4
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-08 13:56:30 +0100

    Create bullet scene
    Bullet node setup and base movement script

---
+++ weapons/bullets/bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+## Speed at which the bullet flies
+@export var speed: float = 1000.0
+## Maximum travel distance of the bullet
+@export var max_range: float = 1500.0
+var distance_traveled: float = 0.0
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Vector2.RIGHT.rotated(rotation)
+	var velocity: Vector2 = direction * speed
+	position += velocity * delta
+	
+	distance_traveled += speed * delta
+	
+	if distance_traveled > max_range:
+		_destroy()
+
+func _destroy() -> void:
+	queue_free()
