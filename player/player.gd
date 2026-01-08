ae073612d80a954e2bd75547c17e99e5ca1be2c0
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-08 11:54:01 +0100

    Add player movement
    Basic player movement and creation of test scene

--- player/player.gd
+++ player/player.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+## Maximum speed allowed for the player character
+@export var max_speed: float = 600.0
+## How much speed is added per seconds to the player speed when a movement key is pressed
+@export var acceleration: float = 1200.0
+## How much speed is lost per seconds when the player release all movement keys
+@export var deceleration: float = 1000.0
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var has_input_direction: bool = direction.length() > 0.0
+	
+	if has_input_direction:
+		var desired_velocity: Vector2 = direction * max_speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+	
+	move_and_slide()
