c81864f8cb376dcafeb2a624874e4d61efd5489b
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-08 16:19:42 +0100

    Add gamepad support to weapon aiming

--- player/weapon_pivot.gd
+++ player/weapon_pivot.gd
@@ -1,5 +1,19 @@
 extends Node2D
 
+var is_using_gamepad: bool = false
+
 func _process(delta: float) -> void:
-	var mouse_direction: Vector2 = global_position.direction_to(get_global_mouse_position())
-	rotation = mouse_direction.angle()
+	var aim_direction: Vector2 = Vector2.ZERO
+	if is_using_gamepad:
+		aim_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	else:
+		aim_direction = global_position.direction_to(get_global_mouse_position())
+
+	if aim_direction.length() > 0.0:
+		rotation = aim_direction.angle()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion or event is InputEventKey:
+		is_using_gamepad = false
+	elif event is InputEventJoypadMotion or event is InputEventJoypadButton:
+		is_using_gamepad = true
