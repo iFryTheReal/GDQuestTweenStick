11f767a2d5abb53c8362ff72f89a0caf0b71989f
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-08 17:23:30 +0100

    Add following routine to mob

--- mobs/mob.gd
+++ mobs/mob.gd
@@ -2,6 +2,13 @@ extends CharacterBody2D
 
 @onready var _detection_area: Area2D = %DetectionArea
 
+## Maximum speed allowed for the mob
+@export var max_speed: float = 600.0
+## How much speed is added per seconds to the mob when it detect the player
+@export var acceleration: float = 700.0
+## How much speed is lost per seconds when the player release all movement keys
+@export var deceleration: float = 700.0
+
 var _player: Player = null
 
 func _ready() -> void:
@@ -18,5 +25,13 @@ func _ready() -> void:
 	)
 
 func _physics_process(delta: float) -> void:
-	if _player != null:
-		pass
+	if _player == null:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+	else:
+		var direction: Vector2 = global_position.direction_to(_player.global_position)
+		var distance: float = global_position.distance_to(_player.global_position)
+		var speed: float = max_speed if distance > 120.0 else max_speed * distance / 120.0
+		var desired_velocity: Vector2 = direction * speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+
+	move_and_slide()	
