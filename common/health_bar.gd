3c82b5cbcef5989990c04dfc430a93fe0565d580
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-12 10:00:05 +0100

    Change value of last point in health bar gradient

--- common/health_bar.gd
+++ common/health_bar.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	health_gradient.colors = PackedColorArray()
 	health_gradient.add_point(0.0, Color(0.631, 0.0, 0.047, 1.0))
 	health_gradient.add_point(0.5, Color(0.717, 0.464, 0.0, 1.0))
-	health_gradient.add_point(0.999, Color(0.188, 0.545, 0.0))
+	health_gradient.add_point(1.0, Color(0.188, 0.545, 0.0))
 	
 func set_health(value: int) -> void:
 	_progress_bar.value = value
