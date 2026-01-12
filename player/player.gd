3c00a99296f65cfb9a28ef90ba9de9e660ab8126
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-12 10:44:13 +0100

    Add function to toggle player control

--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,11 @@
 class_name Player
 extends CharacterBody2D
 
-@onready var _sprite_2d: Sprite2D = %Sprite2D
+@onready var _body_sprite: Sprite2D = %BodySprite
 @onready var _collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var _health_bar: Control = %HealthBar
+@onready var _weapon: Node2D = %Weapon
+@onready var _weapon_pivot: Node2D = %WeaponPivot
 
 ## Maximum speed allowed for the player character
 @export var max_speed: float = 600.0
@@ -27,7 +29,7 @@ func _physics_process(delta: float) -> void:
 	if has_input_direction:
 		var desired_velocity: Vector2 = direction * max_speed
 		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
-		_sprite_2d.rotate_sprite(direction)
+		_body_sprite.rotate_sprite(direction)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 		
@@ -42,11 +44,16 @@ func set_health(new_health: int) -> void:
 func take_damage(damage: int) -> void:
 	health -= damage
 	
-	_sprite_2d.modulate = Color.RED
+	_body_sprite.modulate = Color.RED
 	var tween: Tween = create_tween()
-	tween.tween_property(_sprite_2d, "modulate", Color.WHITE, 0.1)
+	tween.tween_property(_body_sprite, "modulate", Color.WHITE, 0.1)
 
 func die() -> void:
-	set_physics_process(false)
+	toggle_player_control(false)
 	_collision_shape_2d.set_deferred("disabled", true)
 	queue_free()
+
+func toggle_player_control(is_active: bool) -> void:
+	set_physics_process(is_active)
+	_weapon.set_physics_process(is_active)
+	_weapon_pivot.set_process(is_active)
