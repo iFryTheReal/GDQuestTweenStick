464932f1c8b5c4709504007fb7be2c82f6d3963b
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-15 16:21:06 +0100

    Fix pickable item position after opening chest

--- pickups/chest/chest.gd
+++ pickups/chest/chest.gd
@@ -32,11 +32,10 @@ func open() -> void:
 	_animation_player.play("opening")
 	var pickup_item: Pickup = pickup_scene.instantiate()
 	pickup_item.item = possible_items.pick_random()
-	pickup_item.global_position = global_position
 	var spawn_angle: float = randf_range(0.0, 2 * PI)
 	var spawn_distance: float = randf_range(min_spawn_distance, max_spawn_distance)
-	pickup_item.position = position + Vector2.RIGHT.rotated(spawn_angle) * spawn_distance
-	get_tree().root.add_child(pickup_item)
+	var land_position: Vector2 = Vector2.RIGHT.rotated(spawn_angle) * spawn_distance
+	add_child(pickup_item)
 	
 	const FLIGHT_TIME := 0.4
 	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0
@@ -45,15 +44,15 @@ func open() -> void:
 	tween.set_parallel()
 	pickup_item.scale = Vector2(0.25, 0.25)
 	tween.tween_property(pickup_item, "scale", Vector2(1.0, 1.0), HALF_FLIGHT_TIME)
-	tween.tween_property(pickup_item, "position:x", pickup_item.position.x, FLIGHT_TIME)
+	tween.tween_property(pickup_item, "position:x", land_position.x, FLIGHT_TIME)
 
 	tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.set_ease(Tween.EASE_OUT)
 	var jump_height := randf_range(30.0, 80.0)
-	tween.tween_property(pickup_item, "position:y", pickup_item.position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.tween_property(pickup_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
 	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property(pickup_item, "position:y", pickup_item.position.y, HALF_FLIGHT_TIME)
+	tween.tween_property(pickup_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
 
 func _get_configuration_warnings() -> PackedStringArray:
 	return ["\"possible_items\" array is empty"] if possible_items.is_empty() else []
