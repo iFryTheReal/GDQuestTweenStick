ecc7c7c1f1e6fee969ae73406594b112c4d44fb6
iFryTheReal <93728868+iFryTheReal@users.noreply.github.com>
2026-01-15 15:40:58 +0100

    Add theme override to health bar fill

--- common/health_bar.gd
+++ common/health_bar.gd
@@ -13,13 +13,13 @@ func _ready() -> void:
 	health_gradient.add_point(0.0, Color(0.631, 0.0, 0.047, 1.0))
 	health_gradient.add_point(0.5, Color(0.717, 0.464, 0.0, 1.0))
 	health_gradient.add_point(1.0, Color(0.188, 0.545, 0.0))
+	_progress_bar.add_theme_stylebox_override("fill", fill_style)
 	
 func set_health(value: int) -> void:
 	_progress_bar.value = value
 	var health_percent: float = value / float(max_health)
 	fill_style.bg_color = health_gradient.sample(health_percent)
-	_progress_bar.theme.set_stylebox("fill", "ProgressBar", fill_style)
-
+	
 func set_max_health(value: int) -> void:
 	max_health = value
 	_progress_bar.max_value = max_health
